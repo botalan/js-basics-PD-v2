3971fa5808bff53a1607cd42d20648a274b99197
RicardsAlmanis <you@example.com>
2025-11-10 16:23:55 +0200

    coffe-order.js

---
+++ coffee-order.js
@@ -0,0 +1,12 @@
+let coffeeSize = "large";
+let addSugar = true;
+if(coffeeSize === "small"){
+console.log("izmērs maksā 1 EUR")
+}else if(coffeeSize === "medium"){
+    console.log("izmērs maksā 1.5")
+}else{
+    console.log("izmērs maksā 2")
+}
+if(addSugar){
+    console.log("cukura piedeva maksā 0.20 EUR")
+}
